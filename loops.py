91831ab30f3529a92df9054f15d5318a6dd25830
Tahir Abbas <tahir99888plm@gmail.com>
2024-10-07 01:47:25 +0500

    Addition of new file.

---
+++ loops.py
@@ -0,0 +1,7 @@
+# name : str = input("What is your name?")                 # Input from user through console/ terminal.
+# print(type(name))
+# print(f"Welcome Dear {name}!")
+
+fruits = ["Apple", "Banana", "Cherry", "Apricot", "Mango", "Lichi"]
+print(fruits)
+print("The first three elements in the list are:" [0:3])
