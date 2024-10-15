b97a9a3cd3a6a86e7cc6fe57ea792c1f7eba767b
Tahir Abbas <tahir99888plm@gmail.com>
2024-10-15 16:41:14 +0500

    Delete loops.py

--- loops.py
+++
@@ -1,7 +0,0 @@
-# name : str = input("What is your name?")                 # Input from user through console/ terminal.
-# print(type(name))
-# print(f"Welcome Dear {name}!")
-
-fruits = ["Apple", "Banana", "Cherry", "Apricot", "Mango", "Lichi"]
-print(fruits)
-print("The first three elements in the list are:" [0:3])
